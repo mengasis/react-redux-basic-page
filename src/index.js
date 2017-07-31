647da7b5713daa0c9e9c5b5ab9f410ecb4792d40
Manuel Venegas <mengasis@outlook.com>
2017-07-31 18:52:26 -0400

    Config Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
+import AppRoutes from './routes'
+import configureStore from './configureStore'
+import rootReducer from './reducers'
+
+const store = configureStore({
+	initialState: window.initialState
+}, rootReducer)
+
+
+
+ReactDOM.render(
+	<Provider store={store}>
+		<BrowserRouter>
+			<AppRoutes />
+		</BrowserRouter>
+	</Provider>,
+	document.getElementById('root'))
+	
 registerServiceWorker()
---
+++ src/routes.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+// Components
+import App from './components/App'
+//import History from './components/History'
+//import Resumen from './components/Resumen'
+//import Mythology from './components/Mythology'
+//import Page404 from './components/Page404'
+
+// Container
+//import Home from './containers/Home'
+
+const AppRoutes = () =>
+	<App>
+		{/*
+		<Switch>
+			<Route exact path="/history" component={History} />
+			<Route exact path="/resumen" component={Resumen} />
+			<Route exact path="/mythology/gods" component={Mythology} />
+			<Route exact path="/mythology/god/:id" component={Mythology} />
+			<Route exact path="/" component={Home} />
+			<Route component={Page404} />
+		</Switch>
+		*/}
+	</App>
+
+export default AppRoutes
