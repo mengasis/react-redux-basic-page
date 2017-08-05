0f7a11907e80947b97225c98b0c7c9fbb3ec8f24
Manuel Venegas <mengasis@outlook.com>
2017-08-05 17:07:40 -0400

    Added greeting buttons with Redux

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Grid, Row, Col, Button, ButtonToolbar } from 'react-bootstrap'
+import { Grid, Row, Col, Button } from 'react-bootstrap'
 
 import * as actions from '../../actions/homeAction'
 
@@ -9,6 +9,35 @@ class Home extends Component {
 
 	constructor(props){
 		super(props)
+
+		this.handleClick = this.handleClick.bind(this)
+	}
+
+	handleClick(e){
+		e.preventDefault()
+		//this.props.setTitleHome(text)
+
+		const idiomas = 
+		{
+			español: 'Saludando en Español',
+			ingles: 'Saying hello in English', 
+			griego: 'χαιρετισμό Αγγλικά'
+		}
+
+		const key = e.target.attributes.getNamedItem('data-key').value
+
+		switch (key) {
+		case '1':
+			this.props.setTitleHome(idiomas.español)
+			break
+		case '2':
+			this.props.setTitleHome(idiomas.ingles)
+			break
+		case '3':
+			this.props.setTitleHome(idiomas.griego)
+			break
+		}
+
 	}
 
 	render() {
@@ -20,13 +49,25 @@ class Home extends Component {
 			<Grid>
 				<Row>
 					<Col xs={4} md={4}>					
-						<Button bsStyle="primary" block>Hola</Button>
+						<Button 
+							bsStyle="primary"
+							data-key={1}
+							onClick={this.handleClick}
+							block>Hola</Button>
 					</Col>
 					<Col xs={4} md={4}>					
-						<Button bsStyle="success" block>Hello</Button>
+						<Button 
+							bsStyle="success"
+							data-key={2}
+							onClick={this.handleClick}
+							block>Hello</Button>
 					</Col>
 					<Col xs={4} md={4}>					
-						<Button bsStyle="info" block>Γεια σας</Button>
+						<Button 
+							bsStyle="info" 
+							data-key={3}
+							onClick={this.handleClick}
+							block>Γεια σας</Button>
 					</Col>
 				</Row>
 			</ Grid>
